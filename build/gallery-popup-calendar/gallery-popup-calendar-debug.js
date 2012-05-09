6da0412c021378af0676a9a92cef371480f82b6c
Jeff Pihach <hatched@fromanegg.com>
2012-05-09 14:47:55 -0600

    added in date normalization for cross browser iso support

--- build/gallery-popup-calendar/gallery-popup-calendar-debug.js
+++ build/gallery-popup-calendar/gallery-popup-calendar-debug.js
@@ -37,10 +37,7 @@ Y.PopupCalendar = Y.Base.create('popup-calendar', Y.Calendar, [Y.WidgetPosition,
         }
 
         if (minDataDate) {
-            minDate = new Date(minDataDate);
-            if (minDate.toString() == "Invalid Date") {
-                minDate = new Date();
-            }
+            minDate = this.normalizeIsoDate(minDataDate);
             this.set('startDate', minDate);
             this.set('minimumDate', minDate);
 
@@ -50,7 +47,7 @@ Y.PopupCalendar = Y.Base.create('popup-calendar', Y.Calendar, [Y.WidgetPosition,
         }
 
         if (maxDataDate) {
-            maxDate = new Date(maxDataDate);
+            maxDate = this.normalizeIsoDate(maxDataDate);
             this.set('maximumDate', maxDate);
             this.set('endDate', maxDate);
         }
@@ -59,6 +56,26 @@ Y.PopupCalendar = Y.Base.create('popup-calendar', Y.Calendar, [Y.WidgetPosition,
             rules: this._buildDisabledRule(),
             filterFunction: this.filterFunction
         });
+    },
+
+    /*
+     * Normalizes the date for cross browser support
+     *
+     * @method normalizeIsoDate
+     * @public
+     */
+    normalizeIsoDate: function(date) {
+        Y.log('normalizeIsoDate', 'info', this.name);
+        var dateString = date.replace(/-/g, '/'),
+            normalizedDate;
+
+        normalizedDate = new Date(dateString);
+
+        if (normalizedDate == "Invalid Date" || isNaN(normalizedDate)) {
+            return new Date();
+        }
+
+        return normalizedDate
     },
 
     /*
